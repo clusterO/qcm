e6493e66d3dc6b67a4147921d749ce459312a1c8
cluster0 <fourmou.m@gmail.com>
2020-02-20 14:08:33 +0100

    Check get access control

--- formstats/app/Controllers/Home.php
+++ formstats/app/Controllers/Home.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c3541a8b321dd75d962a0ee9f76bdad4a62f76b9accd863c279feea61e1f7dcd
-size 1584
+oid sha256:950ec903a68a5e14268d0e87c578730234ec5c692297345fe4706f8a67b089a0
+size 1607
---
+++ formstats/app/Helpers/UserCheckHelper.php
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f07c14f12347eb18edda39c5fbd74c0c44dae9bce44abd364a0a9ebf9e8a7a93
+size 449
