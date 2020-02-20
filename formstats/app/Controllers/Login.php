44edc2c4c38141968924153a58d64ba5fdf08221
cluster0 <fourmou.m@gmail.com>
2020-02-20 17:42:19 +0100

    logout user

--- formstats/app/Controllers/Login.php
+++ formstats/app/Controllers/Login.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ec9010ac274697da3714616c71557661b5c219d6ae7ab2db1151b404859bf72d
-size 4232
+oid sha256:4f3558919531f8937c93eb252fdfcfe5c55f1c44ec42a0c26900b5fa29e61a0b
+size 3759
--- formstats/app/Helpers/UserCheckHelper.php
+++ formstats/app/Helpers/UserCheckHelper.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b9ef151e7fc70d9d5123dc46f7a4bc18fcf8a89c34cb12933107144bdf6348c9
-size 3704
+oid sha256:9617901eef35581668b0bf4a8d55eed6c8a23c280529ceaccb7d1d9a4cbe7b2b
+size 3949
