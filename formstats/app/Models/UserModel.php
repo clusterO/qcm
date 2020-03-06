942911149f246234ce7e843f6404cf222367e4f2
cluster0 <fourmou.m@gmail.com>
2020-03-06 13:11:55 +0100

    moving user model to builder class

--- formstats/app/Models/UserModel.php
+++ formstats/app/Models/UserModel.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8452f6f8e792bd29e195b4c334595a60a14a8866a572cff5d06e718fb306060f
-size 656
+oid sha256:655be09422a735e9de54dc47a840850e815df4f344df53074f0eae6d15fe8f7f
+size 654
