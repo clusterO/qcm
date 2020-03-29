1b74953f0c3aaa2e131f149a78fa1832dc0a62a9
cluster0 <fourmou.m@gmail.com>
2020-03-29 12:50:39 +0100

    NG update method in the api is deprecated and need a reimplementation

--- formstats/app/Controllers/Api.php
+++ formstats/app/Controllers/Api.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bfd7d5f9362c10059fa05bf02942b3e540d2a9fddf0b92a6eed19ca39e86dc0b
-size 5751
+oid sha256:a6b083bb2bf2191e260d78433ccb694ea9b0c15f01273eb2ff801485aea7a93f
+size 5827
